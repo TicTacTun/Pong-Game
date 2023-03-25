1051849c0e97d7d4d4151d678b43c8d9340fa96d
TicTacTun <kadir.hasan@g.bracu.ac.bd>
2023-03-25 20:35:38 +0600

    Update timer

--- pong.py
+++ pong.py
@@ -1,18 +1,21 @@
 import pygame, sys, random
 
 def ball_animation():
-  global ball_speed_x, ball_speed_y, player_score, opponent_score
+  global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
   ball.x += ball_speed_x
   ball.y += ball_speed_y
     
   if ball.top <= 0 or ball.bottom >= height:
     ball_speed_y *= -1 # Reverse the ball speed when collide with the border
+  
   if ball.left <= 0: 
     player_score += 1
-    ball_restart()
+    score_time = pygame.time.get_ticks()
+  
   if ball.right >= width:
     opponent_score += 1
-    ball_restart() # If ball hit any left/right wall, it will reset
+    score_time = pygame.time.get_ticks()
+  
   if ball.colliderect(player) or ball.colliderect(opponent):
     ball_speed_x *= -1 # Reverse the ball speed when collide with the sticks
 
@@ -34,12 +37,29 @@ def opponent_animation():
     opponent.bottom = height
 
 def ball_restart():
-  global ball_speed_x, ball_speed_y
-  ball.center = (width/2, height/2)
-  ball_speed_y *= random.choice((1,-1)) 
-  ball_speed_x *= random.choice((1,-1)) # After restart, ball starts with random pos
-    
+  global ball_speed_x, ball_speed_y, current_time, score_time
 
+  current_time = pygame.time.get_ticks()
+  ball.center = (width/2, height/2)
+
+  if current_time - score_time < 700:
+    countdown_three = game_font.render("Ready", False, light_grey)
+    screen.blit(countdown_three, (width/2 - 30, height/2 + 20))
+  if 700 < current_time - score_time < 1400:
+    countdown_two = game_font.render(" Set", False, light_grey)
+    screen.blit(countdown_two, (width/2 - 30, height/2 + 20))
+  if 1400 < current_time - score_time < 2100:
+    countdown_one = game_font.render("  Go", False, light_grey)
+    screen.blit(countdown_one, (width/2 - 30, height/2 + 20))
+
+  if current_time - score_time < 2100:
+    ball_speed_x = 0 
+    ball_speed_y = 0
+  else:
+    ball_speed_y = 7 * random.choice((1,-1)) 
+    ball_speed_x = 7 * random.choice((1,-1)) # After restart, ball starts with random pos
+    score_time = None
+    
 # General setup
 pygame.init() # initiate all the pygame module
 clock =  pygame.time.Clock()
@@ -66,7 +86,10 @@ opponent_speed = 7 # Change the value to change difficulty
 # Text Variables
 player_score = 0
 opponent_score = 0
-game_font = pygame.font.Font("Koulen-Regular.ttf", 48) # (font name, size)
+game_font = pygame.font.Font("E:\Python\pygame\Pong-Game\Koulen-Regular.ttf", 32) # (font name, size)
+
+# Timer
+score_time = True
 
 while True:
     # Handling the input
@@ -96,6 +119,9 @@ while True:
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (width/2, 0), (width/2, height)) # (surface, color, tuple of startpoint, tuple of endpoint)
+
+    if score_time:
+      ball_restart()
 
     player_text = game_font.render(f"{player_score}", False, light_grey) # (argument, True/False, color)
     screen.blit(player_text,(670,460))
